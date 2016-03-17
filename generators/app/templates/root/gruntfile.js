1b99f176050584b4b02d44923e63762c383eb109
Adrien LAUER <adrien.lauer@gmail.com>
2016-03-17 11:25:58 +0100

    Dependency cleanup

--- generators/app/templates/root/gruntfile.js
+++ generators/app/templates/root/gruntfile.js
@@ -1,15 +1,7 @@
 /* global module: false, grunt: false, process: false */
-var requirejs = require('requirejs'),
-    fs = require('fs'),
-    _ = require('lodash');
-
 module.exports = function (grunt) {
     'use strict';
 
-    /*
-    * Individual grunt tasks.
-    *
-    **/
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: [
@@ -52,10 +44,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-coveralls');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
